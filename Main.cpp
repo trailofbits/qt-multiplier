2c65824f3886a31b7259e825fd980ddc0680f057
Peter Goodman <peter.goodman@gmail.com>
2022-07-20 14:17:10 -0400

    Force dark palette always

--- Main.cpp
+++ Main.cpp
@@ -73,6 +73,23 @@ int main(int argc, char *argv[]) {
 
   if (getenv("MX_NO_CUSTOM_THEME") == nullptr) {
     qApp->setStyle(config.style);
+    QPalette dark_palette;
+    dark_palette.setColor(QPalette::Window, QColor(53,53,53));
+    dark_palette.setColor(QPalette::WindowText, Qt::white);
+    dark_palette.setColor(QPalette::Base, QColor(25,25,25));
+    dark_palette.setColor(QPalette::AlternateBase, QColor(53,53,53));
+    dark_palette.setColor(QPalette::ToolTipBase, Qt::white);
+    dark_palette.setColor(QPalette::ToolTipText, Qt::white);
+    dark_palette.setColor(QPalette::Text, Qt::white);
+    dark_palette.setColor(QPalette::Button, QColor(53,53,53));
+    dark_palette.setColor(QPalette::ButtonText, Qt::white);
+    dark_palette.setColor(QPalette::BrightText, Qt::red);
+    dark_palette.setColor(QPalette::Link, QColor(42, 130, 218));
+
+    dark_palette.setColor(QPalette::Highlight, QColor(42, 130, 218));
+    dark_palette.setColor(QPalette::HighlightedText, Qt::black);
+
+    qApp->setPalette(dark_palette);
   }
 
   // Per Josh Hofing:
