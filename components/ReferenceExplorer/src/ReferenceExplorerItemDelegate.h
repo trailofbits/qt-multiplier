2ee6e4dabe562812dd0c9a3f4f3d1b8a1db5b0ea
Alessandro Gario <alessandro.gario@gmail.com>
2023-02-27 18:28:45 +0100

    ReferenceExplorer: Add missing documentation

--- components/ReferenceExplorer/src/ReferenceExplorerItemDelegate.h
+++ components/ReferenceExplorer/src/ReferenceExplorerItemDelegate.h
@@ -12,25 +12,34 @@
 
 namespace mx::gui {
 
+//! A delegate used by the IReferenceExplorer widget to draw nodes
 class ReferenceExplorerItemDelegate final : public QStyledItemDelegate {
   Q_OBJECT
 
  public:
+  //! Constructor
   ReferenceExplorerItemDelegate(QObject *parent = nullptr);
+
+  //! Destructor
   virtual ~ReferenceExplorerItemDelegate() override;
 
+  //! Helps Qt determine what's the ideal QTreeView item size
   virtual QSize sizeHint(const QStyleOptionViewItem &option,
                          const QModelIndex &index) const override;
 
+  //! Draws the item data
   virtual void paint(QPainter *painter, const QStyleOptionViewItem &option,
                      const QModelIndex &index) const override;
 
+  //! Disabled copy constructor
   ReferenceExplorerItemDelegate(const ReferenceExplorerItemDelegate &) = delete;
 
+  //! Disabled copy assignment operator
   ReferenceExplorerItemDelegate &
   operator=(const ReferenceExplorerItemDelegate &) = delete;
 
  protected:
+  //! Triggered when the user tries to edit the QTreeView item
   virtual bool editorEvent(QEvent *event, QAbstractItemModel *model,
                            const QStyleOptionViewItem &option,
                            const QModelIndex &index) override;
