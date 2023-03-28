97eaf76816d9b4d515c95bf0ac33d1cedf612c81
Alessandro Gario <5714290+alessandrogario@users.noreply.github.com>
2023-03-28 19:44:08 +0200

    ReferenceExplorer: Add initial icon support (#123)

--- components/ReferenceExplorer/src/ReferenceExplorerItemDelegate.h
+++ components/ReferenceExplorer/src/ReferenceExplorerItemDelegate.h
@@ -38,6 +38,9 @@ class ReferenceExplorerItemDelegate final : public QStyledItemDelegate {
   ReferenceExplorerItemDelegate &
   operator=(const ReferenceExplorerItemDelegate &) = delete;
 
+  //!
+  static void DrawIcon(QPainter &painter, const int &size, const QString &text);
+
  protected:
   //! Triggered when the user tries to edit the QTreeView item
   virtual bool editorEvent(QEvent *event, QAbstractItemModel *model,
