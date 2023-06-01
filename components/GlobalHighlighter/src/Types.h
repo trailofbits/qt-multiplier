05d1e5293f1ab4389b288634b2a61ff19f07deb9
Alessandro Gario <alessandro.gario@gmail.com>
2023-06-01 16:45:44 -0400

    GlobalHighlighter: Use the primary entity id for navigation
    Fixes #260

--- components/GlobalHighlighter/src/Types.h
+++ components/GlobalHighlighter/src/Types.h
@@ -21,6 +21,7 @@ using EntityColorMap = std::unordered_map<RawEntityId, QColor>;
 
 struct EntityHighlight final {
   QString name;
+  RawEntityId primary_entity_id{};
   QColor color;
   std::unordered_set<RawEntityId> entity_id_list;
 };
