8f353c37337fcda0724c80e984205acf19c0846d
Alessandro Gario <alessandro.gario@gmail.com>
2023-08-24 18:05:28 +0200

    InformationExplorer: Improve theme support

--- components/InformationExplorer/src/InformationExplorerTreeView.cpp
+++ components/InformationExplorer/src/InformationExplorerTreeView.cpp
@@ -56,8 +56,15 @@ void InformationExplorerTreeView::InstallItemDelegate() {
   setItemDelegate(new InformationExplorerItemDelegate());
 }
 
-void InformationExplorerTreeView::OnThemeChange(const QPalette &,
-                                                const CodeViewTheme &) {
+void InformationExplorerTreeView::OnThemeChange(
+    const QPalette &palette, const CodeViewTheme &code_view_theme) {
+  // Do not spawn popups from this widget without restoring the
+  // real application palette first!
+  auto custom_palette = palette;
+  custom_palette.setBrush(QPalette::Base,
+                          QBrush(code_view_theme.default_background_color));
+  setPalette(custom_palette);
+
   InstallItemDelegate();
   update();
 }
