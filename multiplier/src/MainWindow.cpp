372024799f0e6e2eb6f7e1dbb278a01c9491504c
Alessandro Gario <alessandro.gario@gmail.com>
2023-11-21 18:07:52 +0100

    ICodeView: Move the CodeWidget

--- multiplier/src/MainWindow.cpp
+++ multiplier/src/MainWindow.cpp
@@ -8,10 +8,10 @@
 #include "PreviewableReferenceExplorer.h"
 #include "QuickReferenceExplorer.h"
 #include "SimpleTextInputDialog.h"
-#include "CodeWidget.h"
 #include "MxTabWidget.h"
 #include "InformationExplorerWidget.h"
 
+#include <multiplier/ui/CodeWidget.h>
 #include <multiplier/ui/PopupWidgetContainer.h>
 #include <multiplier/ui/DockWidgetContainer.h>
 #include <multiplier/ui/Assert.h>
